e3a8163396df2d76f19a219d30a4362bc3ed1626
matanya alkobi <matanor123456@gmail.com>
2025-06-29 10:19:10 +0300

    Arranging the puzzles into a dynamic mode

--- riddels/index.js
+++ riddels/index.js
@@ -1,6 +1,7 @@
 import riddle1 from "./riddle1.js"; 
 import riddle2 from "./riddle2.js";
 import riddle3 from "./riddle3.js";
-export const rid1 = riddle1;
-export const rid2 = riddle2;
-export const rid3 = riddle3;
+const rid1 = riddle1;
+const rid2 = riddle2;
+const rid3 = riddle3;
+export const allRiddles = [rid1, rid2, rid3]
